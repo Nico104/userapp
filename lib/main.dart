d31d7286e0e6ee1a1e3ccc6041856f572633368c
lepups <nicolascarraro104@gmail.com>
2023-03-03 16:57:56 +0100

    navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:circle_nav_bar/circle_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:userapp/pets/pets.dart';
 
+import 'bottom_nav_bar/chip_style.dart';
+import 'bottom_nav_bar/src/bottom_bar_inspired_inside.dart';
+import 'bottom_nav_bar/tab_item.dart';
+import 'bottom_nav_bar/widgets/inspired/inspired.dart';
 import 'pets/custom_paint_test.dart';
 
 void main() {
@@ -49,12 +52,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
-  int _tabIndex = 1;
-  int get tabIndex => _tabIndex;
-  set tabIndex(int v) {
-    _tabIndex = v;
-    setState(() {});
-  }
+  // int tabIndex = 1;
+  // int _tabIndex = 1;
+  // int get tabIndex => _tabIndex;
+  // set tabIndex(int v) {
+  //   _tabIndex = v;
+  //   setState(() {});
+  // }
+
+  int visit = 0;
 
   double navbarheight = 60;
   double navbarbottompadding = 20;
@@ -64,52 +70,85 @@ class _MyHomePageState extends State<MyHomePage>
   @override
   void initState() {
     super.initState();
-    pageController = PageController(initialPage: _tabIndex);
+    pageController = PageController(initialPage: visit);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBody: true,
-      bottomNavigationBar: CircleNavBar(
-        activeIcons: const [
-          Icon(Icons.person, color: Colors.black),
-          Icon(Icons.home, color: Colors.black),
-          Icon(Icons.favorite, color: Colors.black),
-        ],
-        inactiveIcons: const [
-          Text("My"),
-          Text("Home"),
-          Text("Like"),
-        ],
-        iconCurve: Curves.fastOutSlowIn,
-        iconDurationMillSec: 250,
-        tabDurationMillSec: 250,
-        color: Colors.white,
-        height: 60,
-        circleWidth: 60,
-        activeIndex: tabIndex,
-        onTap: (index) {
-          tabIndex = index;
-          pageController.jumpToPage(tabIndex);
+      // extendBody: true,
+      // bottomNavigationBar: CircleNavBar(
+      //   activeIcons: const [
+      //     Icon(Icons.person, color: Colors.black),
+      //     Icon(Icons.home, color: Colors.black),
+      //     Icon(Icons.favorite, color: Colors.black),
+      //   ],
+      //   inactiveIcons: const [
+      //     Text("My"),
+      //     Text("Home"),
+      //     Text("Like"),
+      //   ],
+      //   iconCurve: Curves.fastOutSlowIn,
+      //   iconDurationMillSec: 250,
+      //   tabDurationMillSec: 250,
+      //   color: Colors.white,
+      //   height: 60,
+      //   circleWidth: 60,
+      //   activeIndex: tabIndex,
+      //   onTap: (index) {
+      //     tabIndex = index;
+      //     pageController.jumpToPage(tabIndex);
+      //   },
+      //   padding:
+      //       EdgeInsets.only(left: 16, right: 16, bottom: navbarbottompadding),
+      //   cornerRadius: const BorderRadius.only(
+      //     topLeft: Radius.circular(8),
+      //     topRight: Radius.circular(8),
+      //     bottomRight: Radius.circular(24),
+      //     bottomLeft: Radius.circular(24),
+      //   ),
+      //   shadowColor: Colors.black12,
+      //   elevation: 8,
+      // ),
+      bottomNavigationBar: BottomBarInspiredInside(
+        // height: 50,
+        items: items,
+        backgroundColor: Colors.white,
+        color: Colors.black,
+        colorSelected: Colors.black,
+        indexSelected: visit,
+        iconSize: 32,
+        activeIconSize: 42,
+        sizeInside: 64,
+        notchBorderColor: Colors.black,
+        notchBorderWidth: 2.5,
+        notchTopMargin: 16,
+        borderColor: Colors.black,
+        // borderColor: Colors.white,
+        borderWidth: 3,
+        elevation: 0,
+        curveSize: 82,
+        onTap: (int index) {
+          setState(() {
+            visit = index;
+          });
+          pageController.jumpToPage(index);
         },
-        padding:
-            EdgeInsets.only(left: 16, right: 16, bottom: navbarbottompadding),
-        cornerRadius: const BorderRadius.only(
-          topLeft: Radius.circular(8),
-          topRight: Radius.circular(8),
-          bottomRight: Radius.circular(24),
-          bottomLeft: Radius.circular(24),
+        chipStyle: const ChipStyle(
+          convexBridge: true,
+          background: Colors.yellow,
+          notchSmoothness: NotchSmoothness.sharpEdge,
         ),
-        shadowColor: Colors.black12,
-        elevation: 8,
+        itemStyle: ItemStyle.circle,
+        animated: true,
+        duration: const Duration(milliseconds: 75),
       ),
       body: PageView(
         physics: const NeverScrollableScrollPhysics(),
         controller: pageController,
-        onPageChanged: (v) {
-          tabIndex = v;
-        },
+        // onPageChanged: (v) {
+        //   tabIndex = v;
+        // },
         children: [
           Container(
               width: double.infinity,
@@ -133,3 +172,18 @@ class _MyHomePageState extends State<MyHomePage>
     );
   }
 }
+
+const List<TabItem> items = [
+  TabItem(
+    icon: Icons.shopping_bag_outlined,
+    // title: 'Shop',
+  ),
+  TabItem(
+    icon: Icons.home_outlined,
+    // title: 'Home',
+  ),
+  TabItem(
+    icon: Icons.settings_outlined,
+    // title: 'Setting',
+  ),
+];
